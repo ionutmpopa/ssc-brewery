08675afcfe2cb62e8cb0340ee0af2a487878bb5f
ionutmpopa <Caragiale6>
2022-07-01 14:50:18 +0300

    Spring Security course, following along, section 15.

--- src/main/java/guru/sfg/brewery/repositories/security/UserRepository.java
+++ src/main/java/guru/sfg/brewery/repositories/security/UserRepository.java
@@ -2,11 +2,16 @@ package guru.sfg.brewery.repositories.security;
 
 import guru.sfg.brewery.domain.security.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
+import java.sql.Timestamp;
+import java.util.List;
 import java.util.Optional;
 
-@Repository
+/**
+ * Created by jt on 6/21/20.
+ */
 public interface UserRepository extends JpaRepository<User, Integer> {
     Optional<User> findByUsername(String username);
+
+    List<User> findAllByAccountNonLockedAndLastModifiedDateIsBefore(Boolean locked, Timestamp timestamp);
 }
